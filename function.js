b8e649b818bb49db276225980bf60e58caaf283b
4ldar <alfathsr08@gmail.com>
2025-10-06 10:09:09 +0700

    First commit

---
+++ function.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio Alfath Ari Wibowo siap!');
+  
+    const nameTitle = document.querySelector('h1');
+    nameTitle.addEventListener('click', () => {
+      alert('Halo! Ini portfolio Alfath Ari Wibowo 🚀');
+    });
+  });
+  
